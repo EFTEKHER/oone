57ee222d2dc76ca258b7e48af59d433fa46fbe2c
EFTEKHER <eftekherali2000@gmail.com>
2024-03-13 03:53:25 +0600

    done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,16 +1,25 @@
 import React,{useEffect,useState} from 'react'
+import axios from 'axios';
 import Coin from './Product';
 const Home = () => {
     const arr=[1,2,3,4];
 const [temp,setTemp]=useState(0);
- useEffect(() =>{
-console.log('Mounting');
-return ()=>{
-  console.log('unmounting');
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
 }
+cors(corsOptions);
+ useEffect(() =>{
 
+const  fetchAllCoins=async()=>{
+  const {data}=await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=bdt&per_page=20')
+  console.log(data);
+}
+fetchAllCoins();
 
- },[temp])   
+ },[])   
   return (
     <div>
     <button onClick={()=>setTemp(temp+1)} >Click</button>
